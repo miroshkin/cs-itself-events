55c429d45dd36b1038d49bff7403dc8af051bbf4
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-08-08 00:57:39 +0300

    Init commit

---
+++ CS.Itself.Event/CS.Itself.Event/Carpathia.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CS.Itself.Event
+{
+    public class Carpathia
+    {
+        public Carpathia()
+        {
+        }
+    }
+}
---
+++ CS.Itself.Event/CS.Itself.Event/Titanic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CS.Itself.Event
+{
+    public class Titanic
+    {
+        public Titanic()
+        {
+        }
+    }
+}
